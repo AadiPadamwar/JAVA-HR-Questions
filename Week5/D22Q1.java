6e296d258518a8179b3dc79b373da50e459f0ed3
AadiPadamwar <95860971+AadiPadamwar@users.noreply.github.com>
2023-03-04 19:28:30 +0530

    Create D22Q1.java

---
+++ Week5/D22Q1.java
@@ -0,0 +1,22 @@
+/*
+A set of employees Joins a new company and they started entering their details such as Name, Age, Experience, Employee-id, Salary. 
+Then Once the HR enter their name he gets the complete details of the employees.
+
+Input Format
+-------------
+Rahul
+
+Constraints
+-----------
+Use Parameterized Constructor to initialize the data members
+Use toString() method to display the details and getName() method to display the name
+
+Output Format
+--------------
+Rahul
+33
+10
+1132
+30000
+*/
+
